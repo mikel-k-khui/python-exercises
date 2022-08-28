29c665859a37de4b649de943f813ed66cd6579aa
Mike Chui <7028879+mikel-k-khui@users.noreply.github.com>
2022-08-28 18:23:33 +0000

    Added check_win function and random choice

--- main.py
+++ main.py
@@ -8,5 +8,19 @@ def get_choices(player_choice = "paper"):
   choices = { "player": player_choice, "computer": random.choice(game_choices) }
   return choices
 
+def check_win(choices):
+  player = choices["player"]
+  computer = choices["computer"]
+  if player == computer:
+    return "It's a tie" 
+  elif player == "rock":
+    return "You Win! Rock smashs scissors" if computer == "scissors" else "You Lost! Paper covers rock"
+  elif player == "paper":
+    return "You Win! Papers cover rock" if computer == "rock" else "You Lost! Scissors cuts paper"
+  elif player == "scissors":
+    return "You Win! Scissors cuts paper" if computer == "paper" else "You Lost! Rock smashs scissors"
+
+
 choices = get_choices(random.choice(game_choices))
 print(choices)
+print(check_win(choices))
