d394b3d6262d1d15b8f570a59fdf7574ad33be90
Mike Chui <7028879+mikel-k-khui@users.noreply.github.com>
2022-08-29 07:12:45 +0000

    quick change to main

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import scripts.rps as rps
 import scripts.fib_recursion as fib
 
 choices = rps.get_choices()
-print(choices)
-print(rps.check_win(choices))
+print(f"Choices were {choices} and {rps.check_win(choices)}")
 
 print(f"The 10th fib seq is {fib.fib_recursion(10)}")
