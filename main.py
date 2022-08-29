8e9b3f2f0a5605aa650d4c093be77d466feadaa5
Mike Chui <7028879+mikel-k-khui@users.noreply.github.com>
2022-08-29 07:10:51 +0000

    added recursive calculation of fibonacci sequence

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import scripts.rps as rps
+import scripts.fib_recursion as fib
 
 choices = rps.get_choices()
 print(choices)
 print(rps.check_win(choices))
+
+print(f"The 10th fib seq is {fib.fib_recursion(10)}")
---
+++ scripts/fib_recursion.py
@@ -0,0 +1,8 @@
+def fib_recursion(k):
+  if(k > 2):
+    result = fib_recursion(k - 2) + fib_recursion(k - 1)
+  elif(k == 2):
+  	result = 1 + fib_recursion(k - 1)
+  elif(k == 1):
+    result = 0
+  return result
