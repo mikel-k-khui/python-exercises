43f5214997a947d73ac3b8e6128a7247bdd9e24b
Mike Chui <7028879+mikel-k-khui@users.noreply.github.com>
2022-08-29 22:16:58 +0000

    added cli input with argparse

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from scripts.rps import get_choices, check_win # import module
 import scripts.fib_recursion as fib # import from files
+import argparse
 
-choices = get_choices()
+# get user input
+parser = argparse.ArgumentParser(description = "print something")
+parser.add_argument('-c', '--cli_choice', metavar='cli_choice', required=False, choices={'rock', 'paper', 'scissors'}, help="Pick Rock, Paper, or Scissors")
+args = parser.parse_args()
+print(args.cli_choice)
+                          
+choices = get_choices(args.cli_choice)
 print(f"Choices were {choices} and {check_win(choices)}")
 
 print(f"The 10th fib seq is {fib.fib_recursion(10)}")
